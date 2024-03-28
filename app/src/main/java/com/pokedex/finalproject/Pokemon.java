1d597f7f9d1e49089f02be61452884147ec7af82
hansnery <hansnery@gmail.com>
2024-03-28 15:20:10 -0400

    Pokemon model to receive JSON object

---
+++ app/src/main/java/com/pokedex/finalproject/Pokemon.java
@@ -0,0 +1,27 @@
+package com.pokedex.finalproject;
+
+import java.util.List;
+
+public class Pokemon {
+    private int id;
+    private String name;
+    private int base_experience;
+    private int height;
+    private boolean is_default;
+    private int order;
+    private int weight;
+    private List<Ability> abilities;
+    private List<Form> forms;
+    private List<GameIndex> game_indices;
+    private List<HeldItem> held_items;
+    private String location_area_encounters;
+    private List<Move> moves;
+    private Species species;
+    private Sprites sprites;
+    private Cries cries;
+    private List<Stat> stats;
+    private List<Type> types;
+    private List<PastType> past_types;
+
+    // Constructors, getters, and setters
+}
